08da6b012a611cca7c83e20993de83e980442973
aaronwilli638 <102252635+aaronwilli638@users.noreply.github.com>
2024-10-15 10:36:09 -0500

    Enviornment

---
+++ Assets/move.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AutoMoveAndRotate moveAndRotate = GetComponent<AutoMoveAndRotate>();
+    }
+}
